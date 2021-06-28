9f5ccbef5094a67f5cb5d129ed00e86648ff52a8
UnicronMilkJurassic <iuriikharkovskiimail.gmail.com>
2021-06-29 00:41:19 +0300

    parser

--- index.js
+++ index.js
@@ -1,61 +1,39 @@
 const got = require('got');
 const cheerio = require('cheerio');
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Kharkovsky21!?',
-    database: 'productfootball'
-});
-
-connection.connect(err => {
-    if(err){
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
-
-function closeConnection () {
-    connection.end(err => {
-        if(err) throw err
-        console.log('Connection closed as id' + connection.threadId);
-    });
-};
+const mysql = require('mysql2/promise');
 
 (async () => {
     try {
         const response = await got('https://www.flashscore.ru/team/chelsea/4fGZN2oK/');
         const $ = cheerio.load(response.body);
 
-        const teamName = $('div.teamHeader__name').text().replace(/\s+/g, '');
+        const teamName = $('div.teamHeader__name').text().trim();
         const country = $('a.breadcrumb__link').eq(-1).text();
 
         const teamLogo = $('div.teamHeader__logo').attr('style');
         const slicedTeamLogo = teamLogo.slice(teamLogo.indexOf('(') + 1, teamLogo.length - 1);
         const logoFinalLook = `https://www.flashscore.ru${slicedTeamLogo}`;
 
-        let checkSQL = `SELECT 1 from teaminfo where teamName = "${teamName}"`
-        connection.query(checkSQL, (err, result) => {
-            if (err) throw err
-            console.log('Result', result)
-            if (result.length !== 0) {
-                console.log(`Команда ${teamName} уже добавлена`)
-                closeConnection()
-            } else {
-                let post = {id: 1, teamName: teamName, country: country, teamLogo: logoFinalLook}
-                let sql = 'INSERT INTO teaminfo SET ?'
-                connection.query(sql, post, (err, result) => {
-                    if (err) throw err
-                    console.log('Команда добавлена!', result)
-                    closeConnection()
-                });
-            }
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'Kharkovsky21!?',
+            database: 'productfootball'
         });
+
+        let checkSQL = 'SELECT 1 from teaminfo where teamName = ?';
+
+        const [answerSQL] = await connection.execute(checkSQL, [teamName]);
+        if (answerSQL.length !== 0) {
+            console.log(`Команда ${teamName} уже есть в таблице`)
+        } else {
+            let sql = 'INSERT INTO teaminfo SET teamName = ?, country = ?, teamLogo = ?'
+            await connection.execute(sql, [teamName, country, logoFinalLook])
+            console.log(`Команда ${teamName} добавлена!`)
+        }
+        await connection.end();
     } catch(e) {
-        console.log(`Error: ${e}`)
+        console.log(`Error: ${e}`);
     }
 })();
 
